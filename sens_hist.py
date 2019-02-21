546f5aa0ed77bf9e5437e9783604769c75c5ab5d
gzdominguez <47695100+gzdominguez@users.noreply.github.com>
2019-02-20 21:44:13 -0800

    Update sens_hist.py

--- sens_hist.py
+++ sens_hist.py
@@ -3,10 +3,8 @@
 This script retrieves and prints the temperature history data for
 a sensor over a specified range.
 To use it, run:
-python sens_hist.py --t <TEMP_TYPE> --b <BEGIN_TIME> --i <INCREMENT> --e<END_TIME>,
+python sens_hist.py --b <BEGIN_TIME> --i <INCREMENT> --e<END_TIME>,
 passing in sensor ID, begin time, increment size, and end time.
-Next steps: 
-1. process temp type arg to determine temperature type (probe or ambient)
 """
 from datetime import datetime
 import calendar
@@ -17,35 +15,32 @@ from samsara.apis import SamsaraClient
 
 
 @click.command()
-@click.argument('--t', type=str, required=True) #sensor ID
-@click.argument('--b', type=int, required=True) #begin time
-@click.argument('--i', type=int, required=True) #increment size
-@click.argument('--e', type=int, required=True) #end time
+@click.option('--b', type=int, required=True) #begin time
+@click.option('--i', type=int, required=True) #increment size
+@click.option('--e', type=int, required=True) #end time
 
 
-def get_sensors_history(t, b, i, e):
+def get_sensors_history(b, i, e):
     # Create an instance of the SamsaraClient.
     client = SamsaraClient()
     # Get a sensor's temperature history at specified range 
-	# Values are multiplied by 1000 to get in ms
+    # Values are multiplied by 1000 to get in ms
     end_ms = e*1000 
     step_ms = i
     start_ms = b*1000
-	sensor_id = 212014918225862
+    sensor_id = 212014918225862
     group_id = 25328
-    series = [{"widgetId": sensor_id, "field": t}]
+    series = [{"widgetId": sensor_id, "field": "ambientTemperature"}]
     fill_missing = "withNull"
     params = samsara.HistoryParam(group_id, start_ms, end_ms, step_ms, series, fill_missing)
-	access_token = "dYmy4DpauFvADRPpwUxXA57HlvzsvM"
+    access_token = "dYmy4DpauFvADRPpwUxXA57HlvzsvM"
     history = client.get_sensors_history(access_token, params)
     #write results to csv
-    with open('/data/temp.csv', mode='w') as temp_csv:
-    temp_writer = csv.writer(temp_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for result in history.results:
-        temp_writer.writerow([result.time_ms, result.series])
+    with open('<path to csv folder should match M query>', mode='w') as temp_csv:
+       temp_writer = csv.writer(temp_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+       for result in history.results:
+            temp_writer.writerow([result.time_ms, result.series])
 
 
 if __name__ == "__main__":
     get_sensors_history()
-
-
